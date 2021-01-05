4abec289618c9ed80a737df9132332071fb50eb0
LAPTOP-1GT2BQSF\erikv <erikvdheide14@gmail.com>
2021-01-05 16:26:10 +0100

    Updated code

--- seminarErikCode.R
+++ seminarErikCode.R
@@ -39,7 +39,6 @@ unique(traffic$country)
 sum(traffic$country == "Netherlands")
 sum(traffic$country == "Belgium")
 
-
 # Descriptive of BROAD
 summary(broad)
 unique(broad$operator)
@@ -55,3 +54,21 @@ unique(broad$program_category_after)
 unique(broad$gross_rating_point)
 unique(broad$product_category)
 unique(broad$country)
+
+# Select one day from data (Wednesday May 1, 2019):
+traffic_day <- subset(traffic, grepl("2019-05-01", traffic$date_time) == TRUE)
+traffic_day <- traffic_day[order(traffic_day$date_time),]
+
+# Agregate visits for every minute of the day 
+# assumption: there is at least 1 visit every minute of the day
+minute_counter = 1 # will go up to about 1440
+visit_density <- vector(mode="integer", length=1440)
+for (i in 2:nrow(traffic_day)) {
+  if (traffic_day$date_time[i] == traffic_day$date_time[i-1]) {
+    visit_density[minute_counter] = visit_density[minute_counter] + 1
+  } else {
+    minute_counter = minute_counter + 1
+  }
+}
+
+plot(visit_density, main = "Number of Visitors on May 1, 2019")
