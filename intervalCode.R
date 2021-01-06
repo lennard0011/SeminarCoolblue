0e5ced64f257310e47f88cc63da844d5748cf6b4
Lennard <lennardplas@gmail.com>
2021-01-06 22:55:10 +0100

    Intervalcode gemaakt om data van visitors voor en na te krijgen. Moet het nog runnen, maar kan richting de 10 uur duren met runnen....

--- intervalCode.R
+++ intervalCode.R
@@ -12,48 +12,34 @@ broad['time_min'] <- 0
 for (index in 1:nBroad) { #nBroad
   time <- broad$time[[index]]
   timeMinute <- 60 * 24 * as.numeric(times(time))
-  broad$time_min[[index]] <- timeSeconds
+  broad$time_min[[index]] <- timeMinute
 }
-#add time_min to every travel
+#add time_min and date to every travel
 traffic['time_min'] <- 0
 traffic$date_time <- as.character(traffic$date_time)
+trafficDateSplitWhole <- strsplit(traffic$date_time, "\\s+")
+trafficDateSplitUnlist <- unlist(trafficDateSplitWhole)
+traffictime <- trafficDateSplitUnlist[seq(2, length(trafficDateSplitUnlist), 2)]
+traffic$date <- trafficDateSplitUnlist[seq(1, length(trafficDateSplitUnlist), 2)]
+traffic$time_min <- 60 * 24 * as.numeric(times(traffictime))
 
-for (indexTravel in 1:nTraffic) {
-  trafficDateSplit <- strsplit(trafficDate, "\\s+")[[1]]
-  trafficDate <- trafficDateSplit[1]
-  trafficTime <- trafficDateSplit[2]
-  
-  traffic$time_min[[indexTravel]] <- 60 * 24 * as.numeric(times(trafficTime))
-  if( indexTravel %% 1000 == 0) { print(indexTravel)}
+#count visits pre-commercial
+broad['preVisitors'] <- 0
+intervalSize <- 10
+start <- Sys.time()
+for (index in 1:nBroad) { #nBroad
+  broadDate <- broad$date[[index]]
+  broadTime <- broad$time_min[[index]]
+  broad$preVisitors[[index]] <- length(which(traffic$date == broadDate & traffic$time_min <= broadTime & traffic$time_min >= broadTime - intervalSize))
+  if(index %% 1000 == 0) {print(Sys.time() - start)}
 }
-
-
-#count visits per minute
-for (index in 1:3) { #nBroad
-  preTrafficcount <- 0
-  date <- as.character(broad$date[[index]])
-  time <- broad$time[[index]]
-  
-  timeSeconds <- 60 * 60 * 24 * as.numeric(times(time))
-  
-  print(timeSeconds)
-  
-  for (indexTravel in 1:1) { #nTraffic
-    trafficDate <- as.character(traffic$date_time[[indexTravel]])
-    trafficDateSplit <- strsplit(trafficDate, "\\s+")[[1]]
-    trafficDate <- trafficDateSplit[1]
-    trafficTime <- trafficDateSplit[2]
-    
-    trafficTimeSeconds <- 60 * 60 * 24 * as.numeric(times(trafficTime))
-    
-    if(date == trafficDate) {
-      print("found!")
-      preTrafficcount = preTrafficcount + 1
-    }
-    
-  }
-   
-  
-  broad$travelBefore[[index]] <- preTrafficcount
+#count visits pre-commercial
+broad['postVisitors'] <- 0
+intervalSize <- 10
+start <- Sys.time()
+for (index in 1:nBroad) { #nBroad
+  broadDate <- broad$date[[index]]
+  broadTime <- broad$time_min[[index]]
+  broad$postVisitors[[index]] <- length(which(traffic$date == broadDate & traffic$time_min >= broadTime & traffic$time_min <= broadTime + intervalSize))
+  if(index %% 1000 == 0) {print(Sys.time() - start)}
 }
-
