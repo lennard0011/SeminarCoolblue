02c1d66e8c1efa5d0a9f07fea07d5055ff5f108e
Lennard <lennardplas@gmail.com>
2021-01-07 17:30:13 +0100

    ols model gemaakt voor direct

--- intervalCode.R
+++ intervalCode.R
@@ -1,5 +1,6 @@
 #change to intervals
 library(chron)
+library(caTools)
 
 traffic <- read.csv(file.choose(), header = T)
 broad <- read.csv(file.choose(), header = T)
@@ -25,21 +26,43 @@ traffic$time_min <- 60 * 24 * as.numeric(times(traffictime))
 
 #count visits pre-commercial
 broad['preVisitors'] <- 0
-intervalSize <- 10
+intervalSize <- 5
 start <- Sys.time()
-for (index in 1:nBroad) { #nBroad
+for (index in 1:500) { #nBroad
   broadDate <- broad$date[[index]]
   broadTime <- broad$time_min[[index]]
-  broad$preVisitors[[index]] <- length(which(traffic$date == broadDate & traffic$time_min <= broadTime & traffic$time_min >= broadTime - intervalSize))
+  extraViews <- 0
+  if(broadTime - intervalSize < 0){
+    extraViews <- length(which(traffic$date == as.Date(broadDate) - 1 & traffic$time_min >= 60*24 - intervalSize))
+  } else{
+    broad$preVisitors[[index]] <- length(which(traffic$date == broadDate & traffic$time_min < broadTime & traffic$time_min >= broadTime - intervalSize)) + extraViews
+  }
   if(index %% 1000 == 0) {print(Sys.time() - start)}
 }
-#count visits pre-commercial
+#count visits post-commercial
 broad['postVisitors'] <- 0
-intervalSize <- 10
 start <- Sys.time()
-for (index in 1:nBroad) { #nBroad
+for (index in 1:500) { #nBroad
   broadDate <- broad$date[[index]]
   broadTime <- broad$time_min[[index]]
-  broad$postVisitors[[index]] <- length(which(traffic$date == broadDate & traffic$time_min >= broadTime & traffic$time_min <= broadTime + intervalSize))
+  broad$postVisitors[[index]] <- length(which(traffic$date == broadDate & traffic$time_min >= broadTime & traffic$time_min < broadTime + intervalSize))
   if(index %% 1000 == 0) {print(Sys.time() - start)}
 }
+
+mean(broad$postVisitors[1:500] - broad$preVisitors[1:500])
+data = cbind(broad$postVisitors[1:500], broad$preVisitors[1:500])
+#data = cbind(log(broad$postVisitors[1:500]), log(broad$preVisitors[1:500]))
+data <- as.data.frame(data)
+colnames(data) <- c("postVisitors", "preVisitors")
+
+data_split = sample.split(data$postVisitors, SplitRatio = 0.8)
+train <- subset(data, data_split == TRUE)
+test <-subset(data, data_split == FALSE)
+plot(data$preVisitors, data$postVisitors)
+lines(cbind(0,10000), cbind(0,10000))
+
+model <- lm(postVisitors ~ 0 + preVisitors, data = train) #DataFlair
+summary(model)
+coefficients(model)
+hist(broad$postVisitors[1:500])
+hist(broad$preVisitors[1:500])
