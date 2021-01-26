7b1b08225e16a166aa4479b1fecac859ede9ffaa
LAPTOP-FJUCHGE2\marj9 <marj99@live.nl>
2021-01-26 15:41:29 +0100

    merge

--- intervalCode.R
+++ intervalCode.R
@@ -121,15 +121,12 @@ summary(simpleModelApp)
 ##            REGRESSION MODELS 2-minute model
 ## ========================================================
 
-<<<<<<< HEAD
 # split data in training and test
 set.seed(21)
 data_split = sample.split(broad$postVisitorsWeb, SplitRatio = 0.8)
 train = subset(broad$postVisitorsWeb, data_split == TRUE)
 test = subset(broad$postVisitorsWeb, data_split == FALSE)
 
-=======
->>>>>>> aec0f8bccc3541f5001d14b8aa5498b081a5cb3f
 # function for model summary
 getModelSumm <- function(model, coef) {
   if(coef) {
@@ -161,19 +158,13 @@ getModelSumm(fullModel, TRUE)
 ##                    Overfitting Test
 ## ========================================================
 
-<<<<<<< HEAD
 # Calculate Mean Squared Prediction Error 
-postVisitors = broadNet$postVisitorsWeb
-preVisitors = broadNet$preVisitorsWeb
-=======
-#Calculate Mean Squared Prediction Error 
-set.seed(21)
 postVisitorsWeb = broadNet$postVisitorsWeb
 preVisitorsWeb = broadNet$preVisitorsWeb
->>>>>>> aec0f8bccc3541f5001d14b8aa5498b081a5cb3f
 hours = broadNet$hours
 broadDumm = cbind(postVisitorsWeb, preVisitorsWeb, hours, dummiesDirectModel)
 
+set.seed(21)
 sampleSplit = sample.split(broadNet$postVisitorsWeb, SplitRatio = 0.8)
 broadTrain = broadDumm[sampleSplit == TRUE,]
 broadTest = broadDumm[sampleSplit == FALSE,]
@@ -189,13 +180,7 @@ rmse(broadTest$postVisitorsWeb, predict(baselineModel, broadTest))
 #rmse(broadTest$postVisitors, predict(treatmentOnlyModel, broadTest))
 
 # Full treatment model
-<<<<<<< HEAD
-FullModelTotal = lm(postVisitors ~ preVisitors + ., data = broadTrain)
-summary(FullModelTotal)
-rmse(broadTest$postVisitors, predict(FullModelTotal, broadTest))
-=======
 fullModel = lm(postVisitorsWeb ~ preVisitorsWeb + ., data = broadTrain)
 getModelSumm(fullModel, FALSE)
 rmse(broadTrain$postVisitorsWeb, predict(fullModel, broadTrain))
 rmse(broadTest$postVisitorsWeb, predict(fullModel, broadTest))
->>>>>>> aec0f8bccc3541f5001d14b8aa5498b081a5cb3f
