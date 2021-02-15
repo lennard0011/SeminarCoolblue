90365dc6c2e503a1dc0582ef8043b1e454f7c331
LAPTOP-FJUCHGE2\marj9 <marj99@live.nl>
2021-02-15 10:30:08 +0100

    hi

--- bsts.R
+++ bsts.R
@@ -2,11 +2,11 @@
 Sys.setenv(TZ = "Europe/Amsterdam")
 mediaMarktTrends = gtrends(keyword = "mediamarkt", geo = "NL", time = "2019-01-01 2019-06-30")
 interestVectorMed = mediaMarktTrends$interest_over_time
-plot(interestVectorMed$hits, type = 'l')
+plot(interestVectorMed$hits, type = 'l', xlab = "Day of the year", ylab = "MediaMarkt searches")
 
 BCCTrends = gtrends(keyword = "bcc", geo = "NL", time = "2019-01-01 2019-06-30")
 interestVectorBCC = BCCTrends$interest_over_time
-plot(interestVectorBCC$hits, type = 'l')
+plot(interestVectorBCC$hits, type = 'l', xlab = "Day of the year", ylab = "BCC searches")
 
 # laptopTrends = gtrends(keyword = "laptop", geo = "NL", time = "2019-01-01 2019-06-30")
 # interestVectorLap = laptopTrends$interest_over_time
@@ -117,8 +117,8 @@ maxFun(70)
 # 211-224
 # website
 # televisions
-set.seed(11)
 data = zoo(cbind(sumVisitIndexNet, sumVisitIndexBel, interestVectorMed[, 2], interestVectorBCC[, 2]), c(1:181))
+set.seed(11)
 commercialBegin = as.Date(42 - 1, origin = "2019-01-01")
 commercialEnd = as.Date(55 - 1, origin = "2019-01-01")
 yday_commercialBegin = yday(commercialBegin)
@@ -139,11 +139,12 @@ entireImpact1.1 = CausalImpact(data, yday_pre.period, yday_post.period, alpha = 
 plot(entireImpact1.1)
 entireImpact1.1$summary
 entireImpact1.1$report
-plot(entireImpact1.1$model$bsts.model, "coef")
+plot(entireImpact1.1$model$bsts.model, "coef", inc = .1)
 
 # 225-310
 # website
 # laptops
+set.seed(11)
 commercialBegin = as.Date(56 - 1, origin = "2019-01-01")
 commercialEnd = as.Date(69 - 1, origin = "2019-01-01")
 yday_commercialBegin = yday(commercialBegin)
@@ -164,11 +165,11 @@ entireImpact1.2 = CausalImpact(data, yday_pre.period, yday_post.period, alpha = 
 plot(entireImpact1.2)
 entireImpact1.2$summary
 entireImpact1.2$report
-plot(entireImpact1.2$model$bsts.model, "coef")
 
 # 311-407
 # website
 # washing machines
+set.seed(11)
 commercialBegin = as.Date(70 - 1, origin = "2019-01-01")
 commercialEnd = as.Date(97 - 1, origin = "2019-01-01")
 yday_commercialBegin = yday(commercialBegin)
@@ -189,11 +190,11 @@ entireImpact1.3 = CausalImpact(data, yday_pre.period, yday_post.period, alpha = 
 plot(entireImpact1.3)
 entireImpact1.3$summary
 entireImpact1.3$report
-plot(entireImpact1.3$model$bsts.model, "coef")
 
 # 520-603
 # website
 # televisions
+set.seed(11)
 commercialBegin = "2019-05-20"
 yday_commercialBegin = yday(commercialBegin)
 pre.period = c("2019-04-22", "2019-05-19")
@@ -213,11 +214,12 @@ entireImpact2 = CausalImpact(data, yday_pre.period, yday_post.period, alpha = 0.
 plot(entireImpact2)
 entireImpact2$summary
 entireImpact2$report
-plot(entireImpact2$model$bsts.model, "coef")
+plot(entireImpact2$model$bsts.model, "coef", inc = .1)
 
 # 617-630
 # website
 # televisions
+set.seed(11)
 commercialBegin = "2019-06-17"
 yday_commercialBegin = yday(commercialBegin)
 pre.period = c("2019-06-04", "2019-06-16")
@@ -237,16 +239,17 @@ entireImpact3 = CausalImpact(data, yday_pre.period, yday_post.period, alpha = 0.
 plot(entireImpact3)
 entireImpact3$summary
 entireImpact3$report
-plot(entireImpact3$model$bsts.model, "coef")
+plot(entireImpact3$model$bsts.model, "coef", inc = .1)
 
 # test for imaginary period
 # 203-210
 # website
+set.seed(11)
 commercialBegin = "2019-02-04"
 yday_commercialBegin = yday(commercialBegin)
-pre.period = c("2019-01-29", "2019-02-02")
+pre.period = c("2019-02-01", "2019-02-05")
 yday_pre.period = yday(pre.period)
-post.period = c("2019-02-03", "2019-02-10")
+post.period = c("2019-02-06", "2019-02-10")
 yday_post.period = yday(post.period)
 
 # seasonality test
@@ -257,8 +260,8 @@ summary(seasTest)  # period not long enough; choose seasonality
 trendTest = trend.test(sumVisitIndexNet[yday_post.period[1]:yday_post.period[2]]) 
 trendTest # trend not identified
 
-entireImpact4 = CausalImpact(data, yday_pre.period, yday_post.period, model.args = list(niter = 10000, nseasons = 7))
+entireImpact4 = CausalImpact(data, yday_pre.period, yday_post.period, alpha = 0.01, model.args = list(niter = 10000, nseasons = 7))
 plot(entireImpact4)
 entireImpact4$summary
 entireImpact4$report
-plot(entireImpact4$model$bsts.model, "coef")
+plot(entireImpact4$model$bsts.model, "coef", inc = .1)
